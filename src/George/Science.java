e63d82ebc8b20cc586424f898402c25a12961809
Kelvin Ly <kelvin.ly1618@gmail.com>
2014-04-01 15:44:41 -0400

    Restructuring source tree. Build probably still broken.

---
+++ src/George/Science.java
@@ -0,0 +1,8 @@
+//Physics engine.
+public class Science implements Subsystem {
+    public void start() {}
+    public GameState onFrame(GameState s) {
+        return s;
+    }
+    public void destroy() {}
+}
