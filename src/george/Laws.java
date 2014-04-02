ecd954819408bf9709c4cdc4f4ba5a5fe0689ae3
Kelvin Ly <kelvin.ly1618@gmail.com>
2014-04-01 22:11:06 -0400

    Added some timing framework

---
+++ src/george/Laws.java
@@ -0,0 +1,14 @@
+package george;
+
+//TODO: Put script support here.
+
+public class Laws extends Subsystem {
+    public void start() {
+    }
+
+    public GameState onFrame(GameState s) {
+    }
+
+    public void destroy() {
+    }
+}
